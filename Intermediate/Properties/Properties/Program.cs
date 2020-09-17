d4ce19b78bf1e57fe7a2127e59f2c05a42e0cae5
Jack Grant <51317475+JGrant97@users.noreply.github.com>
2020-09-17 17:54:15 +0100

    prop

--- Intermediate/Properties/Properties/Program.cs
+++ Intermediate/Properties/Properties/Program.cs
@@ -2,11 +2,14 @@
 
 namespace Properties
 {
+    public class Person
+    {
+        public DateTime Birthdate { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
