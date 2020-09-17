512de89a553654e4fa2c339c49e4df4138246060
Jack Grant <51317475+JGrant97@users.noreply.github.com>
2020-09-17 23:10:49 +0100

    post

---
+++ Intermediate/Classes StackOverflow Post/Classes StackOverflow Post/Post.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Classes_StackOverflow_Post
+{
+    public class Post
+    {
+        private string _title;
+        private string _desc;
+        private DateTime _postedDate;
+        private int _totalVotes;
+
+        public Post()
+        {
+        }
+
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        public string Desc
+        {
+            get { return _desc; }
+            set { _desc = value; }
+        }
+
+        public DateTime PostedDate
+        {
+            get { return _postedDate; }
+            set { _postedDate = value; }
+        }
+
+        public int TotalVotes
+        {
+            get { return _totalVotes; }
+            private set { _totalVotes = value; }
+        }
+
+        public void UpVote()
+        {
+             _totalVotes = _totalVotes + 1;
+        }
+
+        public void DownVote()
+        {
+
+            _totalVotes = _totalVotes - 1;
+        }
+
+    }
+}
---
+++ Intermediate/Classes StackOverflow Post/Classes StackOverflow Post/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Classes_StackOverflow_Post
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var post = new Post();
+            post.Title = "Hello World";
+            post.Desc = "This is my first post";
+            post.PostedDate = DateTime.Now;
+            while(true)
+            {
+                Console.WriteLine("\nTitle: " + post.Title + "\nDescription: " + post.Desc + "\n" + "Total votes: " + post.TotalVotes +
+                 "\nPosted: " + post.PostedDate + "\nYou can upvote or downvote this post by typing upvote or downvote");
+
+                var input = Console.ReadLine();
+
+                if(input.Contains("upvote".ToLower()))
+                {
+                    post.UpVote();
+                }
+                else if (input.Contains("downvote".ToLower()))
+                {
+                    post.DownVote();
+                }
+                else
+                {
+                    throw new ArgumentException("Only upvote or downvote");
+                }
+            }    
+        }
+    }
+}
