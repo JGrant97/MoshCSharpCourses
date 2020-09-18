3e77443d4f0a9f10212dddba8aa334e1dd3c5d8f
Jack Grant <51317475+JGrant97@users.noreply.github.com>
2020-09-18 22:15:13 +0100

    Constructors and inheritance

---
+++ Intermediate/Constructors and Inheritance/Constructors and Inheritance/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Constructors_and_Inheritance
+{
+    public class Car : Vehicle
+    {
+        public Car(string registrationNumber)
+            : base(registrationNumber)
+        {
+            Console.WriteLine("Car is being initialised. {0}", registrationNumber);
+        }
+    }
+}
---
+++ Intermediate/Constructors and Inheritance/Constructors and Inheritance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Constructors_and_Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car("XYZ1234");
+        }
+    }
+}
