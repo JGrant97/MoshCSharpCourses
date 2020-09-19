af92ef920c7ea7f3d46d1196095e5f937537e2a3
Jack Grant <51317475+JGrant97@users.noreply.github.com>
2020-09-19 17:46:19 +0100

    Boxing and unboxing

---
+++ Intermediate/Boxing and Unboxing/Boxing and Unboxing/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Boxing_and_Unboxing
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var list = new ArrayList();
+            list.Add(1);
+            list.Add("Jack");
+            list.Add(DateTime.Today);
+
+            var list2 = new List<int>();
+            var name = new List<string>();
+            name.Add();
+        }
+    }
+}
