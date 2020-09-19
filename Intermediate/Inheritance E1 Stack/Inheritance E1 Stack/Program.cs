2259ffc9dc6843ece607826219e306d6a5aa9803
Jack Grant <51317475+JGrant97@users.noreply.github.com>
2020-09-19 20:15:58 +0100

    stacks

---
+++ Intermediate/Inheritance E1 Stack/Inheritance E1 Stack/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Inheritance_E1_Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+        }
+    }
+}
---
+++ Intermediate/Inheritance E1 Stack/Inheritance E1 Stack/Stack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Inheritance_E1_Stack
+{
+    public class Stack
+    {
+        private List<object> _stackList = new List<object>();
+
+        public void Push(object obj)
+        {
+            if(obj == null)
+            {
+                throw new InvalidOperationException("Object can't be null");
+            }
+            _stackList.Insert(0, obj);
+   
+        }
+
+        public object Pop()
+        {
+            if (_stackList.Count <= 0)
+            {
+                throw new InvalidOperationException("Stack is empty, cannot pop stack");
+
+            }
+
+            object obj = _stackList[0];
+            _stackList.RemoveAt(0);
+            return obj;
+
+        }
+
+        public void Clear()
+        {
+            _stackList.Clear();
+        }
+    }
+}
