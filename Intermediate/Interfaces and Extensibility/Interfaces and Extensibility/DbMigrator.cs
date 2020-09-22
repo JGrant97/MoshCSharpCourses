aac9b3b7ddd3b0e5582ad3df262519500dd736e2
Jack Grant <51317475+JGrant97@users.noreply.github.com>
2020-09-22 18:32:33 +0100

    interfaces and extensibility

---
+++ Intermediate/Interfaces and Extensibility/Interfaces and Extensibility/DbMigrator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Interfaces_and_Extensibility
+{
+    public class DbMigrator
+    {
+        private readonly ILogger _logger;
+        public DbMigrator(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public void Migrate()
+        {
+            _logger.LogInfo("Migration started at {0}" + DateTime.Now);
+            _logger.LogInfo("Migration finished at {0}" + DateTime.Now);
+ 
+        }
+    }
+}
---
+++ Intermediate/Interfaces and Extensibility/Interfaces and Extensibility/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Interfaces_and_Extensibility
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbMigrator = new DbMigrator(new FileLogger(@"D:\My Documents\GitHub\C#\MoshCSharpCourses\Intermediate\Interfaces and Extensibility\log.txt"));
+            dbMigrator.Migrate();
+        }
+    }
+}
