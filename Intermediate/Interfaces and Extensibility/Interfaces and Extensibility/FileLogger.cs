e2f29769184f429e8cd5a8182c60e5a26558511b
Jack Grant <51317475+JGrant97@users.noreply.github.com>
2020-09-22 18:33:05 +0100

    interfaces and extensibility

---
+++ Intermediate/Interfaces and Extensibility/Interfaces and Extensibility/FileLogger.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+
+namespace Interfaces_and_Extensibility
+{
+    public class FileLogger : ILogger
+    {
+        private readonly string _path;
+
+        public FileLogger(string path)
+        {
+            this._path = path;
+        }
+
+        public void LogError(string message)
+        {
+            Log(message, "Error");
+        }
+
+        public void LogInfo(string message)
+        {
+            Log(message, "Info");
+        }
+
+        private void Log(string message, string messageType)
+        {
+            using (var streamWriter = new StreamWriter(_path, true))
+            {
+                streamWriter.WriteLine(messageType + ": " + message);
+                streamWriter.Dispose();
+            }
+        }
+
+    }
+}
