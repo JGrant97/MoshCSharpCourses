8b151dd26d657c269ae0dbf6f5b35d8fd9cf803f
Jack Grant <51317475+JGrant97@users.noreply.github.com>
2020-09-25 18:53:19 +0100

    Lamda Expressions

---
+++ Advanced/Lambda Expression/Lambda Expression/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lambda_Expression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const int factor = 5;
+            Func<int, int> multiplier = n => n * factor;
+            var result =  multiplier(10);
+            Console.WriteLine(result);
+
+            Func<int, int> square = number => number*number;
+            Console.WriteLine(square(5));
+
+            var books = new BookRepository().GetBooks();
+            var cheapBooks = books.FindAll(book => book.Price <10);
+
+            foreach (var book in cheapBooks)
+            {
+                Console.WriteLine(book.Title);
+            }
+        }
+    }
+}
