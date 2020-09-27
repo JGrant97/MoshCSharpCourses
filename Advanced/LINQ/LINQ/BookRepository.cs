88cd228ae917f477f1b949278521a339bd699f01
Jack Grant <51317475+JGrant97@users.noreply.github.com>
2020-09-27 22:22:36 +0100

    linq

---
+++ Advanced/LINQ/LINQ/BookRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ
+{
+    class BookRepository
+    {
+        public IEnumerable<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book() {Title = "ADO.Net step by step", Price = 5},
+                new Book() {Title = "ASP.Net MVC", Price = 9.99f},
+                new Book() {Title = "ASP.Net Web API", Price = 12},
+                new Book() {Title = "C# Advanced topics", Price = 7},
+                new Book() {Title = "C# Advanced topics", Price = 9}
+
+            };
+        }
+    }
+}
--- Advanced/LINQ/LINQ/Program.cs
+++ Advanced/LINQ/LINQ/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace LINQ
 {
@@ -6,7 +9,42 @@ namespace LINQ
     {
         static void Main(string[] args)
         {
+            var books = new BookRepository().GetBooks();
 
+
+            //Linq query operators
+            var cheaperBooks = 
+                from b in books
+                where b.Price < 10
+                orderby b.Title
+                select b;
+
+            //Linq extension methods
+            var cheapestBooks = books
+                                    .Where(b => b.Price < 10)
+                                    .OrderBy(b => b.Title)
+                                    .Select(b => b.Title);
+
+            var single = books.First(b => b.Title == "C# Advanced topics");
+            Console.WriteLine(single.Title + " " + single.Price +  "\n");
+
+            var count = books.Count();
+            Console.WriteLine(count);
+
+            var max = books.Max(b => b.Price);
+            Console.WriteLine(max);
+
+            var min = books.Max(books => books.Price);
+            Console.WriteLine(min);
+
+            var totalPrices = books.Sum(b => b.Price);
+            Console.WriteLine(totalPrices);
+
+            foreach (var book in cheapestBooks)
+            {
+                // Console.WriteLine(book.Title + " " + book.Price);
+                Console.WriteLine(book);
+            }
         }
     }
 }
