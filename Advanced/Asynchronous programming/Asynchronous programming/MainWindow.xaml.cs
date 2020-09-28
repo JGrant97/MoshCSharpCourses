bb4acf0b403eb0880a99eb3f379398976b2fd8ba
Jack Grant <51317475+JGrant97@users.noreply.github.com>
2020-09-28 23:02:08 +0100

    Asynchronous programming

---
+++ Advanced/Asynchronous programming/Asynchronous programming/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Asynchronous_programming
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            //DownloadHtmlAsync("https://www.google.co.uk");
+            var html = await GetHtmlAsync("https://www.google.co.uk");
+
+            MessageBox.Show(html.Substring(0, 10));
+        }
+
+        public async Task DownloadHtmlAsync(string url)
+        {
+            var webClient = new WebClient();
+            var html = await webClient.DownloadStringTaskAsync(url);
+
+            using (var streamWriter = new StreamWriter(@"D:\Downloads"))
+            {
+                await streamWriter.WriteAsync(html);
+            }
+        }
+
+        public async Task<string> GetHtmlAsync(string url)
+        {
+            var webClient = new WebClient();
+
+            return await webClient.DownloadStringTaskAsync(url);
+        }
+    }
+}
